0717bab7faabf7920b9cb571bbf204ece229296c
j-fborges <contato.joao.pfb@gmail.com>
2025-10-26 19:33:36 -0300

    feat: Setup java project on intelliJ

---
+++ mod5/src/br/com/j_fborges/PrimeiraClasse.java
@@ -0,0 +1,17 @@
+package br.com.j_fborges;
+
+public class PrimeiraClasse {
+
+    public static void main(String args[]) {
+        String amb1 = args[0];
+        String amb2 = args[1];
+        String amb3 = args[2];
+
+        System.out.println("Hello World");
+        System.out.println("Hello" + " " + amb1);
+        System.out.println(amb1);
+        System.out.println(amb2);
+        System.out.println(amb3);
+
+    }
+}
