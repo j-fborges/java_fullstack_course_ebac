198dd2aea30889e0091d3756ec03eaf6032df8f8
j-fborges <contato.joao.pfb@gmail.com>
2025-10-29 12:20:29 -0300

    feat: Cast a primitive variable to its wrapper

---
+++ tarefa_wrapper/src/Main.java
@@ -0,0 +1,15 @@
+import br.com.j_fborges.ValueCaster;
+
+/**
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome");
+
+        ValueCaster caster = new ValueCaster();
+        caster.printCastVariable();
+    }
+}
+
---
+++ tarefa_wrapper/src/br/com/j_fborges/ValueCaster.java
@@ -0,0 +1,13 @@
+package br.com.j_fborges;
+
+public class ValueCaster {
+    public void printCastVariable() {
+
+        System.out.println("Printing ");
+        double var = 3.3345678;
+        System.out.println("Before Casting: " + var);
+        Double var_ = var;
+        System.out.println("After Casting " + var_);
+
+    }
+}
